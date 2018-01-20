56d1dd551f07d35f2ed5a743dfc94d0f2b35290d
Trisha Garrett <trisha.garrett@gmail.com>
2018-01-20 00:07:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :array
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.collect do |element|
+      if element == @word
+        element
+      else
+        array = []
+      end
+    end
+    array
+  end
+
+
+end
